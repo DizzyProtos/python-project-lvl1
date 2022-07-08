574069dd1969624bb47c65ad2a9d383124fe176f
dizzyprotos <medvedcpp@gmail.com>
2022-07-08 14:59:46 +0700

    don't use eval in brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,7 +1,17 @@
 from random import randint
 
 
-HELLO_STRING = 'What is the result of the expression?'
+HELLO_MESSAGE = 'What is the result of the expression?'
+
+
+def _calculate_answer(first_num, second_num, math_symbol):
+    if math_symbol == '+':
+        return first_num + second_num
+    elif math_symbol == '-':
+        return first_num - second_num
+    elif math_symbol == '*':
+        return first_num * second_num
+    raise NotImplementedError(f'Math symbol {math_symbol} is not implemented')
 
 
 def get_question_with_answer():
@@ -11,7 +21,7 @@ def get_question_with_answer():
     math_symbols = ['+', '-', '*']
     operation_choice = randint(0, len(math_symbols) - 1)
     symb = math_symbols[operation_choice]
-    correct_answer = eval('{0}{1}{2}'.format(fst_num, symb, sec_num))
+    correct_answer = _calculate_answer(fst_num, sec_num, symb)
 
     question = '{0} {1} {2}'.format(fst_num, symb, sec_num)
     return question, correct_answer
